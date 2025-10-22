c90547872d734aafe5c17bcc12d2c0572f2a3aa4
BoraComert <boramcomert@gmail.com>
2025-10-22 15:21:19 +0300

    added category

--- relations.sql
+++ relations.sql
@@ -9,3 +9,15 @@ FOREIGN KEY (pod_name) REFERENCES PODCASTS(pod_name),
 FOREIGN KEY (ep_id) REFERENCES EPISODES(ep_id)
 
 );
+
+CREATE TABLE Pod_Belongs_to_Cat (
+
+pod_name VARCHAR(100),
+category_name VARCHAR(100),
+PRIMARY KEY (pod_name,category_name),
+FOREIGN KEY (pod_name) REFERENCES PODCASTS(pod_name),
+FOREIGN KEY (category_name) REFERENCES CATEGORIES(category_name)
+
+
+
+);
