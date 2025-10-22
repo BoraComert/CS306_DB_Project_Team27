e2c8cbf097d4372a7129e764e876d18975580b7b
BoraComert <boramcomert@gmail.com>
2025-10-22 15:15:30 +0300

    added relations

---
+++ relations.sql
@@ -0,0 +1,11 @@
+
+USE  supodcast_db;
+
+CREATE TABLE Pod_has_Ep (
+pod_name VARCHAR(100),
+ep_id int,
+PRIMARY KEY (pod_name,ep_id)
+FOREIGN KEY (pod_name) REFERENCES PODCASTS(pod_name),
+FOREIGN KEY (ep_id) REFERENCES EPISODES(ep_id)
+
+);
